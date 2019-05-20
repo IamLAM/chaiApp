c443cecbfebd7524a2a4d354948eae2b0e498876
Glitch (chaiapp) <none>
2019-05-20 08:19:24 +0000

    🍵🚙 again zombie.js

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -272,16 +272,26 @@ suite('Functional Tests', function() {
       
       /** Try it again... No help this time **/
       test('submit "surname" : "Vespucci" - write your e2e test...', function(done) {
-
-        // fill the form, and submit.
-        // assert that status is OK 200
+ browser
+          .fill('surname', 'Vespucci')
+          .pressButton('submit', function(){
+            browser.assert.success();
+            // assert that the text inside the element 'span#name' is 'Marco'
+            browser.assert.text('span#name', 'Amerigo');
+            // assert that the text inside the element 'span#surname' is 'Polo'
+            browser.assert.text('span#surname', 'Vespucci');
+            // assert that the element(s) 'span#dates' exist and their count is 1
+            browser.assert.element('span#dates', 1);
+            // pressButton is Async.  Waits for the ajax call to complete...
         // assert that the text inside the element 'span#name' is 'Amerigo'
         // assert that the text inside the element 'span#surname' is 'Vespucci'
         // assert that the element(s) 'span#dates' exist and their count is 1
-        assert.fail();
+
         done();
       
       });
     });
   });
-});
+});});
+
+
